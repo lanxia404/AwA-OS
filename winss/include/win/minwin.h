f1c44c1958fd4de5c4e6bc61d800fae06ca8a04e
lanxia404 <lanxia657@gmail.com>
2025-08-27 03:13:18 +0800

    Update minwin.h

--- winss/include/win/minwin.h
+++ winss/include/win/minwin.h
@@ -1,40 +1,116 @@
 #pragma once
 #include <stdint.h>
+#include <stddef.h>
+
+/* --- 基本 Windows 風格型別/巨集（最小集合） --- */
+
+#ifndef WINAPI
+#  if defined(__i386__)
+#    define WINAPI __attribute__((stdcall))
+#  else
+#    define WINAPI /* ignored on non-i386 */
+#  endif
+#endif
 
 typedef uint32_t DWORD;
-typedef int32_t BOOL;
-typedef void* HANDLE;
-typedef uint32_t UINT;
-#ifndef WINAPI
-#define WINAPI __attribute__((stdcall))
-#endif
-#define TRUE 1
-#define FALSE 0
-#define STD_INPUT_HANDLE  ((DWORD)-10)
-#define STD_OUTPUT_HANDLE ((DWORD)-11)
-#define STD_ERROR_HANDLE  ((DWORD)-12)
+typedef int32_t  BOOL;
+typedef uint16_t WORD;
+typedef unsigned char BYTE;
 
-/* 基本指標型別（如你已有可略） */
+#ifndef TRUE
+#  define TRUE 1
+#endif
+#ifndef FALSE
+#  define FALSE 0
+#endif
+
+typedef void*   HANDLE;
+
 #ifndef LPVOID
-typedef void* LPVOID;
+typedef void*   LPVOID;
 #endif
 #ifndef LPDWORD
-typedef DWORD* LPDWORD;
+typedef DWORD*  LPDWORD;
 #endif
-#ifndef LPSTR
-typedef char* LPSTR;
+#ifndef LPBYTE
+typedef BYTE*   LPBYTE;
 #endif
-#ifndef LPCSTR
+
+typedef char*       LPSTR;
 typedef const char* LPCSTR;
-#endif
+
+/* 注意：為了相容 Windows 寬字元，我們讓 WCHAR=16-bit */
+typedef unsigned short WCHAR;
+typedef WCHAR*       LPWSTR;
+typedef const WCHAR* LPCWSTR;
+
 #ifndef UINT
 typedef unsigned int UINT;
 #endif
 
-/* 等待/常數 */
-#ifndef INFINITE
-#define INFINITE 0xFFFFFFFFu
+/* 標準 Handle 常數 */
+#ifndef STD_INPUT_HANDLE
+#  define STD_INPUT_HANDLE  ((DWORD)-10)
+#  define STD_OUTPUT_HANDLE ((DWORD)-11)
+#  define STD_ERROR_HANDLE  ((DWORD)-12)
 #endif
+
+/* Wait/Process 常數 */
+#ifndef INFINITE
+#  define INFINITE 0xFFFFFFFFu
+#endif
+#ifndef WAIT_OBJECT_0
+#  define WAIT_OBJECT_0 0u
+#endif
+#ifndef WAIT_TIMEOUT
+#  define WAIT_TIMEOUT 0x00000102u
+#endif
+#ifndef WAIT_FAILED
+#  define WAIT_FAILED 0xFFFFFFFFu
+#endif
+#ifndef STILL_ACTIVE
+#  define STILL_ACTIVE 259u
+#endif
+
+/* 最小 STARTUPINFO/PROCESS_INFORMATION 結構（欄位未必皆使用） */
+typedef struct _STARTUPINFOA {
+  DWORD cb;
+  LPSTR lpReserved;
+  LPSTR lpDesktop;
+  LPSTR lpTitle;
+  DWORD dwX, dwY, dwXSize, dwYSize;
+  DWORD dwXCountChars, dwYCountChars, dwFillAttribute;
+  DWORD dwFlags;
+  WORD  wShowWindow;
+  WORD  cbReserved2;
+  LPBYTE lpReserved2;
+  HANDLE hStdInput;
+  HANDLE hStdOutput;
+  HANDLE hStdError;
+} STARTUPINFOA;
+
+typedef struct _STARTUPINFOW {
+  DWORD cb;
+  LPWSTR lpReserved;
+  LPWSTR lpDesktop;
+  LPWSTR lpTitle;
+  DWORD dwX, dwY, dwXSize, dwYSize;
+  DWORD dwXCountChars, dwYCountChars, dwFillAttribute;
+  DWORD dwFlags;
+  WORD  wShowWindow;
+  WORD  cbReserved2;
+  LPBYTE lpReserved2;
+  HANDLE hStdInput;
+  HANDLE hStdOutput;
+  HANDLE hStdError;
+} STARTUPINFOW;
+
+typedef struct _PROCESS_INFORMATION {
+  HANDLE hProcess;
+  HANDLE hThread;
+  DWORD  dwProcessId;
+  DWORD  dwThreadId;
+} PROCESS_INFORMATION;#endif
 #ifndef WAIT_OBJECT_0
 #define WAIT_OBJECT_0 0u
 #endif
