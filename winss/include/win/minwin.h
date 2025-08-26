e840d0b6ccb187cd44c869bb808714007d1a9dd9
lanxia404 <lanxia657@gmail.com>
2025-08-27 01:18:19 +0800

    Update minwin.h

--- winss/include/win/minwin.h
+++ winss/include/win/minwin.h
@@ -13,3 +13,15 @@ typedef uint32_t UINT;
 #define STD_INPUT_HANDLE  ((DWORD)-10)
 #define STD_OUTPUT_HANDLE ((DWORD)-11)
 #define STD_ERROR_HANDLE  ((DWORD)-12)
+
+#ifndef LPVOID
+typedef void* LPVOID;
+#endif
+
+#ifndef LPDWORD
+typedef DWORD* LPDWORD;
+#endif
+
+#ifndef LPCVOID
+typedef const void* LPCVOID;
+#endif
