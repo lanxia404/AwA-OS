eb448b320ce4969cdd5573a43f868eb468045c88
lanxia404 <lanxia657@gmail.com>
2025-08-27 14:54:00 +0800

    Update teb.h

--- winss/include/nt/teb.h
+++ winss/include/nt/teb.h
@@ -1,15 +1,20 @@
 #pragma once
 #include <stdint.h>
 
+#ifdef __GNUC__
+#define AWA_EXPORT __attribute__((visibility("default")))
+#else
+#define AWA_EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* 設定目前執行緒的 TEB/TLS（32-bit Linux: 透過 set_thread_area + %fs） */
-void _nt_teb_setup_for_current(void);
-
-/* 取得當前 TEB 基底（純除錯用途，可不一定使用） */
-void* _nt_teb_base(void);
+/* 設定目前執行緒的 TEB/TLS（i386 Linux: set_thread_area + %fs） */
+AWA_EXPORT void  _nt_teb_setup_for_current(void);
+/* 取得目前 TEB 基底（除錯用） */
+AWA_EXPORT void* _nt_teb_base(void);
 
 #ifdef __cplusplus
 }
