4dc6a041f1a4a4381a9c2ff4f9d8518682b32a86
lanxia404 <lanxia657@gmail.com>
2025-08-27 23:52:32 +0800

    Update ntshim_api.h

--- winss/ntshim32/ntshim_api.h
+++ winss/ntshim32/ntshim_api.h
@@ -1,20 +1,17 @@
-#ifndef AWA_NTSHIM_API_H
-#define AWA_NTSHIM_API_H
+#pragma once
+#include "../include/win/minwin.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/* Loader 會把自己的 spawn 實作註冊進來 */
+typedef int (*pe32_spawn_fn)(const char* path, const char* cmdline /*可為NULL*/);
 
-void nt_set_command_lineA(const char* path, const char* argv /*可為 NULL*/);
+/* 設定目前行程的「Windows」命令列（供 GetCommandLineA 使用） */
+void nt_set_command_lineA(const char* s);
+
+/* Loader 註冊用：把 pe32_spawn 的實作塞進來 */
+void nt_set_spawn_impl(pe32_spawn_fn fn);
+
+/* 供 ntshim32 呼叫，用來建立子 PE32 行程 */
+int  pe32_spawn(const char* path, const char* cmdline /*可為NULL*/);
+
+/* 供 Loader 設定 TEB/TLS（你既有實作）*/
 void nt_teb_setup_for_current(void);
-
-/* 由 loader 在進入 PE 前初始化當前執行緒的 TEB/TLS 最小狀態 */
-void nt_teb_setup_for_current(void);
-
-// 由 loader 提供的 in-proc spawn（CreateProcessA 最小實作會呼叫）
-int pe32_spawn(const char* app, const char* cmdline, PROCESS_INFORMATION* pi);
-
-#ifdef __cplusplus
-}
-#endif
-#endif
