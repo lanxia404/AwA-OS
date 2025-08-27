1107929cfc60ebeded6fc84ceef66d3575b34879
lanxia404 <lanxia657@gmail.com>
2025-08-27 14:27:07 +0800

    Update hooks.h

--- winss/include/nt/hooks.h
+++ winss/include/nt/hooks.h
@@ -1,4 +1,18 @@
-#pragma once
-/* 與 loader / ntshim32 共用的 Hook 介面 */
+#ifndef AWA_NT_HOOKS_H
+#define AWA_NT_HOOKS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 輕量 Hook 介面：dll 名、符號名、函式指標 */
 struct Hook { const char* dll; const char* name; void* fn; };
-extern struct Hook NT_HOOKS[];
+
+/* 以函式返回掛鉤表頭指標（以 {NULL,NULL,NULL} 為終止）。 */
+__attribute__((visibility("default")))
+const struct Hook* nt_get_hooks(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* AWA_NT_HOOKS_H */
