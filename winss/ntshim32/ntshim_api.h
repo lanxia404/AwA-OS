27f1d22bb9e7479d3e89747275eb9730ee1ddb3e
lanxia404 <lanxia657@gmail.com>
2025-08-27 23:39:00 +0800

    Update ntshim_api.h

--- winss/ntshim32/ntshim_api.h
+++ winss/ntshim32/ntshim_api.h
@@ -11,8 +11,8 @@ void nt_teb_setup_for_current(void);
 /* 由 loader 在進入 PE 前初始化當前執行緒的 TEB/TLS 最小狀態 */
 void nt_teb_setup_for_current(void);
 
-/* 由 loader 暴露的內部啟動器（shim 會呼叫它） */
-int pe32_spawn(const char* path, const char* argv);
+// 由 loader 提供的 in-proc spawn（CreateProcessA 最小實作會呼叫）
+int pe32_spawn(const char* app, const char* cmdline, PROCESS_INFORMATION* pi);
 
 #ifdef __cplusplus
 }
